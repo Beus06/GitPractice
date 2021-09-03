9c19a0a2eecc713f82e6f245d3d758dd409413f0
TTHLrnd <lorandtoth.lori@gmail.com>
2021-09-03 19:40:46 +0200

    Add instance

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,23 @@
 package main;
 
+import main.model.animals.Animal;
+import main.model.animals.Cat;
+import main.model.animals.Dog;
+import main.model.animals.SeaCucumber;
+import main.model.humanoids.Humanoid;
+
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        // TODO
+        Humanoid FikuszKukisz = new Humanoid();
+        FikuszKukisz.getNewPet(new Dog());
+        FikuszKukisz.getNewPet(new Dog());
+        FikuszKukisz.getNewPet(new Cat());
+        Random randomNum = new Random();
+        for (int i = 0; i < randomNum.nextInt(100); i++) {
+            FikuszKukisz.getNewPet(new SeaCucumber());
+        }
     }
 }
