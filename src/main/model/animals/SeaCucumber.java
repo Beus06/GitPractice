a2b609df32bbba7fc52d7a5c1da76b46964ddb07
TTHLrnd <lorandtoth.lori@gmail.com>
2021-09-03 19:33:59 +0200

    New animal added

---
+++ src/main/model/animals/SeaCucumber.java
@@ -0,0 +1,9 @@
+package main.model.animals;
+
+public class SeaCucumber extends Animal {
+
+    @Override
+    public void makeSound() {
+        System.out.println("Makes sea cucumber noises in the voice of David Hasselhoff");
+    }
+}
