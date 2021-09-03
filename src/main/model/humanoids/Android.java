301c2b21bb63e6792031d187b682e402391529cf
Beus06 <kerteszbeata9@gmail.com>
2021-09-03 19:35:31 +0200

    new class SuperHero

--- src/main/model/humanoids/Android.java
+++ src/main/model/humanoids/Android.java
@@ -6,7 +6,7 @@ public class Android extends Humanoid {
 
 
 
-    
+
     public double getVersion() {
         return version;
     }
---
+++ src/main/model/humanoids/SuperHero.java
@@ -0,0 +1,11 @@
+package main.model.humanoids;
+
+import main.model.animals.Animal;
+
+public class SuperHero extends Humanoid{
+
+    @Override
+    public void getNewPet(Animal animal) {
+        super.getNewPet(animal);
+    }
+}
